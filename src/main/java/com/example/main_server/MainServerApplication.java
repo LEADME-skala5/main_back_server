ee5ec2800bf5937319eacab8081b60b15e879643
soohee Jang <shjang0820@hansung.ac.kr>
2025-05-26 17:05:47 +0900

    feat: DB 연결

--- src/main/java/com/example/main_server/MainServerApplication.java
+++ src/main/java/com/example/main_server/MainServerApplication.java
@@ -2,7 +2,9 @@ package com.example.main_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
+@PropertySource("classpath:properties/env.properties")
 @SpringBootApplication
 public class MainServerApplication {
 
