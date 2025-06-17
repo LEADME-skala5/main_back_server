dd0b24626cd0ed09eb7ea20045d44bc156c7ea9f
SanghyunLee <lgw9736@naver.com>
2025-06-17 19:12:13 +0900

    feat: 직무 Entity 추가

--- src/main/java/com/example/main_server/common/entity/Department.java
+++ src/main/java/com/example/main_server/common/entity/Department.java
@@ -17,7 +17,7 @@ public class Department extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
 
     @Column(name = "name", nullable = false, length = 100)
     private String name;
