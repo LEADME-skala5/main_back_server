bc772c21bfcf71d322ff3cf84b589f0d9b013212
SanghyunLee <lgw9736@naver.com>
2025-06-18 16:40:45 +0900

    chore: table 이름 수정

--- src/main/java/com/example/main_server/evaluation/quantitative/entity/WeeklyEvaluation.java
+++ src/main/java/com/example/main_server/evaluation/quantitative/entity/WeeklyEvaluation.java
@@ -13,6 +13,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -25,6 +26,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "weekly_evaluations")
 public class WeeklyEvaluation extends BaseEntity {
 
     @Id
