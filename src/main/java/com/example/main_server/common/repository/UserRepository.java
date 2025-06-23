105fba67355ab619de6f19336be2f674d9394a4f
SanghyunLee <lgw9736@naver.com>
2025-06-23 14:53:50 +0900

    refactor: 리팩토링
    - 이전 비효율적인 문제 해결

--- src/main/java/com/example/main_server/common/repository/UserRepository.java
+++ src/main/java/com/example/main_server/common/repository/UserRepository.java
@@ -4,6 +4,8 @@ import com.example.main_server.common.entity.User;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -19,4 +21,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
     List<User> findByOrganizationId(Long organizationId);
 
     List<User> findAllByOrganizationId(Long organizationId);
+
+    @Query("SELECT u FROM User u JOIN FETCH u.organization JOIN FETCH u.job WHERE u.organization.id = :orgId")
+    List<User> findByOrganizationIdWithDetails(@Param("orgId") Long orgId);
+
 }
